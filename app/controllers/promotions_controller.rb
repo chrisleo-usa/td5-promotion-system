53af87052a91fc57ffa18810130a00dbd754b763
christopher alves <chrisleo.usa@gmail.com>
2021-01-27 20:30:34 -0300

    Promotion system finalizado

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -4,4 +4,9 @@ class PromotionsController < ApplicationController
     # Se não for expor a variável promotions para a view, então não usamos o @. Como queremos expor ela para a views, então é necessário o @promotions
 
   end
+
+  def show
+    id = params[:id]
+    @promotion = Promotion.find(id)
+  end
 end
