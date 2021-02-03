b04bb860a1caee2363e04c4a6c228a5dd3176eac
christopher alves <chrisleo.usa@gmail.com>
2021-02-03 16:44:15 -0300

    WIP create category

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
-  resources :categories, only: [:index, :show, :new, :create]
   resources :promotions, only: [:index, :show, :new, :create] do 
     post 'generate_coupons', on: :member  #mesma coisa do código abaixo, mas este é o jeito Padrão
   end
+
+  resources :categories, only: [:index, :new]
   # post '/promotions/:id/generate', to: 'promotions#generate_coupons'
 end
--- spec/features/admin_registers_a_category_spec.rb
+++ spec/features/admin_registers_a_category_spec.rb
@@ -5,20 +5,39 @@ feature 'Admin registers a product category' do
     visit root_path
     click_on 'Categorias'
 
-    expect(page).to have_link('Cadastrar uma categoria', 
-                              href: new_category_path)
+    expect(page).to have_link('Cadastrar categoria', href: new_category_path)
   end
 
-  scenario 'succesfully' do 
+  scenario 'succesfully' do
     visit root_path
     click_on 'Categorias'
-    click_on 'Cadastrar uma categoria'
+    click_on 'Cadastrar categoria'
 
-    fill_in 'Categoria', with: 'Tecnologia'
-    click_on 'Criar categoria'
+    fill_in 'Nome', with: 'Tecnologia'
+    fill_in 'Código', with: 'TEC15'
 
-    expect(current_path).to eq(category_path(Category.last)) #Verificar sobre essa rota. 
+    expect(current_path).to eq(categories_path)
     expect(page).to have_content('Tecnologia')
-    expect(page).to have_link('Voltar')
+    expect(page).to have_content('TEC15')
   end
+  # scenario 'from index page' do
+  #   visit root_path
+  #   click_on 'Categorias'
+
+  #   expect(page).to have_link('Cadastrar uma categoria', 
+  #                             href: new_category_path)
+  # end
+
+  # scenario 'succesfully' do 
+  #   visit root_path
+  #   click_on 'Categorias'
+  #   click_on 'Cadastrar uma categoria'
+
+  #   fill_in 'Categoria', with: 'Tecnologia'
+  #   click_on 'Criar categoria'
+
+  #   expect(current_path).to eq(category_path) #Verificar sobre essa rota. 
+  #   expect(page).to have_content('Tecnologia')
+  #   expect(page).to have_link('Voltar')
+  # end
 end
